66f19b519b48bd1d12e0ee8b17fc9b99ee33ebcb
Isaac Muniz <im.isaac.muniz@gmail.com>
2026-01-17 19:36:20 -0300

    urls call views that returns http responses

--- app/urls.py
+++ app/urls.py
@@ -19,7 +19,14 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+
+def cars_view(request):
+    return HttpResponse("My cars")
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("cars/", cars_view),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
