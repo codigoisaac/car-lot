aa4a2adcae1f80639a4f10c8a1b59aaafa45aaaf
Isaac Muniz <im.isaac.muniz@gmail.com>
2026-01-18 17:52:19 -0300

    use a ModelForm instead of just a Form

--- cars/forms.py
+++ cars/forms.py
@@ -1,27 +1,8 @@
 from django import forms
-from cars.models import Brand, Car
+from cars.models import Car
 
 
-class CarForm(forms.Form):
-    model = forms.CharField(max_length=200)
-    brand = forms.ModelChoiceField(Brand.objects.all())
-    manufacture_year = forms.IntegerField()
-    model_year = forms.IntegerField()
-    plate = forms.CharField(max_length=10)
-    value = forms.FloatField()
-    photo = forms.ImageField()
-
-    def create(self):
-        car = Car(
-            model=self.cleaned_data["model"],
-            brand=self.cleaned_data["brand"],
-            manufacture_year=self.cleaned_data["manufacture_year"],
-            model_year=self.cleaned_data["model_year"],
-            plate=self.cleaned_data["plate"],
-            value=self.cleaned_data["value"],
-            photo=self.cleaned_data["photo"],
-        )
-
-        car.save()
-
-        return car
+class CarModelForm(forms.ModelForm):
+    class Meta:
+        model = Car
+        fields = "__all__"
