96f8e2e14c2bc444b6525bee751635469ebd1389
Isaac Muniz <im.isaac.muniz@gmail.com>
2026-01-20 18:30:50 -0300

    use generic view instead of just base view

--- cars/views.py
+++ cars/views.py
@@ -2,18 +2,25 @@ from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
 from django.views import View
+from django.views.generic import ListView
 
 
-class CarsView(View):
-    def get(self, request):
-        search = request.GET.get("search")
+class CarsView(ListView):
+    model = Car
+    template_name = "cars.html"
+    context_object_name = "cars"
+
+    def get_queryset(self):
+        search = self.request.GET.get("search")
 
         if search:
-            cars = Car.objects.filter(model__contains=search).order_by("model")
+            cars = (
+                super().get_queryset().filter(model__contains=search).order_by("model")
+            )
         else:
-            cars = Car.objects.all().order_by("model")
+            cars = super().get_queryset().order_by("model")
 
-        return render(request, "cars.html", {"cars": cars})
+        return cars
 
 
 class NewCarView(View):
