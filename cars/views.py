182eb24fa0cda9d139d8e8478f120b68f4f5cefa
Isaac Muniz <im.isaac.muniz@gmail.com>
2026-01-20 17:12:37 -0300

    use class based view for the cars app

--- cars/views.py
+++ cars/views.py
@@ -1,21 +1,28 @@
 from django.shortcuts import render, redirect
 from cars.models import Car
 from cars.forms import CarModelForm
+from django.views import View
 
 
-def cars_view(request):
-    search = request.GET.get("search")
+class CarsView(View):
+    def get(self, request):
+        search = request.GET.get("search")
 
-    if search:
-        cars = Car.objects.filter(model__contains=search).order_by("model")
-    else:
-        cars = Car.objects.all().order_by("model")
+        if search:
+            cars = Car.objects.filter(model__contains=search).order_by("model")
+        else:
+            cars = Car.objects.all().order_by("model")
 
-    return render(request, "cars.html", {"cars": cars})
+        return render(request, "cars.html", {"cars": cars})
 
 
-def new_car_view(request):
-    if request.method == "POST":
+class NewCarView(View):
+    def get(self, request):
+        new_car_form = CarModelForm()
+
+        return render(request, "new_car.html", {"new_car_form": new_car_form})
+
+    def post(self, request):
         new_car_form = CarModelForm(request.POST, request.FILES)
 
         if new_car_form.is_valid():
@@ -23,7 +30,4 @@ def new_car_view(request):
 
             return redirect("cars")
 
-    else:
-        new_car_form = CarModelForm()
-
-    return render(request, "new_car.html", {"new_car_form": new_car_form})
+        return render(request, "new_car.html", {"new_car_form": new_car_form})
