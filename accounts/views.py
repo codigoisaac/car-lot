6af19fefafbf8dffc864a2a4c97ccaf8026c5d18
Isaac Muniz <im.isaac.muniz@gmail.com>
2026-01-20 13:02:57 -0300

    use django's UserCreationForm

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 
 def register_view(request):
-    return render(request, "register.html")
+    user_form = UserCreationForm()
+
+    return render(request, "register.html", {"user_form": user_form})
