c5501cbdd0cb563d054b98ffe354a83820a602b3
Isaac Muniz <im.isaac.muniz@gmail.com>
2026-01-25 11:47:20 -0300

    create CarDetailView, a subclass of Djangos DetailView

--- app/urls.py
+++ app/urls.py
@@ -19,15 +19,16 @@ from django.contrib import admin
 from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
-from cars.views import CarsView, NewCarView
+from cars.views import CarsView, NewCarView, CarDetailView
 from accounts.views import register_view, login_view, logout_view
 
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("cars/", CarsView.as_view(), name="cars"),
-    path("new_car/", NewCarView.as_view(), name="new_car"),
     path("register/", register_view, name="register"),
     path("login/", login_view, name="login"),
     path("logout/", logout_view, name="logout"),
+    path("cars/", CarsView.as_view(), name="cars"),
+    path("new_car/", NewCarView.as_view(), name="new_car"),
+    path("car/<int:pk>/", CarDetailView.as_view(), name="car_detail"),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
