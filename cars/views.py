519ec61c19a3bf802a2b78a5f00bd152d44972d3
Isaac Muniz <im.isaac.muniz@gmail.com>
2026-01-26 19:16:14 -0300

    delete cars feature

--- cars/views.py
+++ cars/views.py
@@ -1,6 +1,12 @@
 from cars.models import Car
 from cars.forms import CarModelForm
-from django.views.generic import ListView, CreateView, DetailView, UpdateView
+from django.views.generic import (
+    ListView,
+    CreateView,
+    DetailView,
+    UpdateView,
+    DeleteView,
+)
 
 
 class CarsView(ListView):
@@ -37,4 +43,10 @@ class CarUpdateView(UpdateView):
     model = Car
     form_class = CarModelForm
     template_name = "car_update.html"
+    success_url = "/cars/"
+
+
+class CarDeleteView(DeleteView):
+    model = Car
+    template_name = "car_delete.html"
     success_url = "/cars/"
